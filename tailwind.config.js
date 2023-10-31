c7ce60b41025fd8d81c4b1aff68dfa90f2387bbb
Orton Bright <ortonb110@gmail.com>
2023-10-31 19:10:19 +0000

    Project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        defaultDark: "#050505",
+        lightDark: "#1F1F1F",
+        darkGray: "#2D2D2D",
+        defaultGray: "#3A3A3A",
+        lightGray: "#757575",
+        veryLightGray: "#E9E9E9",
+        offWhite: "#F4F4F4",
+        lightPurple: "#A445ED",
+        lightOrange: "#FF5252",
+      },
+      fontFamily: {
+        
+      }
+    },
   },
   plugins: [],
-}
+};
